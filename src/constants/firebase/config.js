d2454a126c4589b5d78ee824c9858197fa035056
Luke Liu <cacocacoon@gmail.com>
2016-11-30 11:53:28 +0800

    delete config

--- src/constants/firebase/config.js
+++ src/constants/firebase/config.js
@@ -1,7 +1,7 @@
 export default {
-	apiKey: "AIzaSyCg5A6-c3ejlls60UgZJ6vdcBNuVHA1sSw",
-	authDomain: "test-3ef32.firebaseapp.com",
-	databaseURL: "https://test-3ef32.firebaseio.com",
-	storageBucket: "test-3ef32.appspot.com",
-	messagingSenderId: "156491155322"
+	apiKey: "",
+	authDomain: "",
+	databaseURL: "",
+	storageBucket: "",
+	messagingSenderId: ""
 };
