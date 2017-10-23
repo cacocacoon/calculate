5c33f9ccffdff42fc39068ad55b0746bef06f57a
luke.liu <cacocacoon@gmail.com>
2017-10-23 15:22:33 +0800

    install Rxjs and modify index.html

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+// import { Button, WhiteSpace, WingBlank } from 'antd-mobile'
+// import 'antd-mobile/dist/antd-mobile.css'
+import { Observable } from 'rxjs/Rx'
 
 
-class App extends React.Component<{}, {}> {
-    render(): JSX.Element {
-        return <div>Hello World!</div>
-    }
+
+const observable = Observable.create(function subscription(observer) {
+	const intervalID = setInterval(() => {
+		observer.next('hi')
+	}, 1000)
+
+	return function unsubscribe() {
+		clearInterval(intervalID)
+	}
+})
+
+
+class App extends React.Component<null, null> {
+	render(): JSX.Element {
+		return <div />
+	}
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.querySelector('main'))
