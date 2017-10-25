444e0896d313bac2457bea716a04091d6995cd58
luke.liu <cacocacoon@gmail.com>
2017-10-25 11:24:03 +0800

    modify app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import './app.scss'
 import * as Rx from 'rxjs'
 import { autobind } from 'core-decorators'
+import './app.scss'
 // import { Button, WhiteSpace, WingBlank } from 'antd-mobile'
 // import 'antd-mobile/dist/antd-mobile.css'
 
@@ -23,32 +23,32 @@ class App extends React.PureComponent<P, S> {
 	}
 
 	private mouseDown(): void {
-		this.pressEventSubscription = this.pressEvent.subscribe(() => {
-			this.setState(prevState => ({
-				score: prevState.score + 1
-			}))
-		})
+		this.pressEventSubscription = this.pressEvent.subscribe(this.click)
 	}
 
 	private mouseUp(): void {
 		this.pressEventSubscription.unsubscribe()
 	}
 
+	private click(): void {
+		this.setState(prevState => ({
+			score: prevState.score + 1
+		}))
+	}
+
 	public componentDidMount(): void {
 		this.pressEvent = Rx.Observable.create(observer => {
-			setInterval(() => {
-				observer.next()
-			}, 300)
+			setInterval(() => observer.next(), 100)
 		})
 	}
 
 	public render(): JSX.Element {
 		return (
-			<div onMouseDown={this.mouseDown} onMouseUp={this.mouseUp}>
+			<div onClick={this.click} onMouseDown={this.mouseDown} onMouseUp={this.mouseUp}>
 				{this.state.score}
 			</div>
 		)
 	}
 }
 
-ReactDOM.render(<App />, document.querySelector('main'))
+ReactDOM.render(<App />, document.getElementsByTagName('main')[0])
