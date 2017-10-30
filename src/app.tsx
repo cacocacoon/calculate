f72a9df05259c04f812cfce0056560b9ef21acc4
luke.liu <cacocacoon@gmail.com>
2017-10-30 11:38:28 +0800

    refactor app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,54 +1,44 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import * as Rx from 'rxjs'
-import { autobind } from 'core-decorators'
-import './app.scss'
-// import { Button, WhiteSpace, WingBlank } from 'antd-mobile'
-// import 'antd-mobile/dist/antd-mobile.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as Rx from 'rxjs';
+import { autobind } from 'core-decorators';
 
-type Props = null
+import './app.scss';
+
+type Props = {};
 
 type State = {
-	score: number
-}
+	score: number;
+};
 
 @autobind
 class App extends React.PureComponent<Props, State> {
-	private pressEvent: Rx.Observable<null> = null
-
-	private pressEventSubscription: Rx.Subscription = null
-
 	public state: State = {
 		score: 0
+	};
+
+	private readonly mouseUp = Rx.Observable.fromEvent(document.querySelector('html'), 'mouseup');
+
+	private mouseDown() {
+		Rx.Observable
+			.interval(100)
+			.takeUntil(this.mouseUp)
+			.subscribe(this.click);
 	}
 
-	private mouseDown(): void {
-		this.pressEventSubscription = this.pressEvent.subscribe(this.click)
-	}
-
-	private mouseUp(): void {
-		this.pressEventSubscription.unsubscribe()
-	}
-
-	private click(): void {
+	private click() {
 		this.setState(prevState => ({
 			score: prevState.score + 1
-		}))
+		}));
 	}
 
-	public componentDidMount(): void {
-		this.pressEvent = Rx.Observable.create(observer => {
-			setInterval(() => observer.next(), 100)
-		})
-	}
-
-	public render(): JSX.Element {
+	public render() {
 		return (
-			<div onClick={this.click} onMouseDown={this.mouseDown} onMouseUp={this.mouseUp}>
+			<div id="app" onClick={this.click} onMouseDown={this.mouseDown}>
 				{this.state.score}
 			</div>
-		)
+		);
 	}
 }
 
-ReactDOM.render(<App />, document.getElementsByTagName('main')[0])
+ReactDOM.render(<App />, document.querySelector('main'));
